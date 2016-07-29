cbc4631f18bfc7f8dbfedf52efc548f5fd90dab2
buckie <wjmartino@gmail.com>
2016-07-29 02:03:42 +0000

    changed the scripts to make things easier
    Signed-off-by: buckie <wjmartino@gmail.com>

--- servers.sh
+++ servers.sh
@@ -6,27 +6,27 @@ cd aws-conf
 cmd="$1"
 case $cmd in
   config)
-    for i in `cat junoservers.privateIp`; do scp -i ~/user.pem -r $i/* ec2-user@$i:; done
+    for i in `cat junoservers.privateIp`; do scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/user.pem -r $i/* ec2-user@$i: & done
     exit 0
     ;;
   start)
-    for i in `cat junoservers.privateIp`; do ssh -i ~/user.pem ec2-user@$i './start.sh'; done
+    for i in `cat junoservers.privateIp`; do ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/user.pem ec2-user@$i './start.sh' & done
     exit 0
     ;;
   stop)
-    for i in `cat junoservers.privateIp`; do ssh -i ~/user.pem ec2-user@$i 'pkill junoserver'; done
+    for i in `cat junoservers.privateIp`; do ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/user.pem ec2-user@$i 'pkill junoserver' & done
     exit 0
     ;;
   copyLogs)
-    for i in `cat junoservers.privateIp`; do scp -i ~/user.pem -r ec2-user@$i:./${i}-output.log ~/juno/cluster-logs/${i}-output.log; done
+    for i in `cat junoservers.privateIp`; do scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/user.pem -r ec2-user@$i:./${i}-output.log ~/juno/cluster-logs/${i}-output.log & done
     exit 0
     ;;
   clearLogs)
-    for i in `cat junoservers.privateIp`; do ssh -i ~/user.pem ec2-user@$i 'rm ./'$i'-output.log'; done
+    for i in `cat junoservers.privateIp`; do ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/user.pem ec2-user@$i 'rm ./'$i'-output.log' & done
     exit 0
     ;;
   ps)
-    for i in `cat junoservers.privateIp`; do ssh -i ~/user.pem ec2-user@$i 'pgrep junoserver'; done
+    for i in `cat junoservers.privateIp`; do ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/user.pem ec2-user@$i 'pgrep junoserver' & done
     exit 0
     ;;
   *)

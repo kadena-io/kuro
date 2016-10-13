d2fdd79b5713957dac8118052fd32db8264d71e3
buckie <wjmartino@gmail.com>
2016-10-13 17:09:46 -0400

    develop: moved away from `stack install` to symlinks Signed-off-by: buckie <wjmartino@gmail.com>

--- demo/start-no-persistence.sh
+++ demo/start-no-persistence.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-rm ./log/*.sqlite
+rm ./log/*.sqlite ./log/10000.log ./log/10001.log ./log/10002.log ./log/10003.log
 tmux new-window
 tmux split-window -h
-tmux send-keys 'stack exec -- kadenaserver +RTS -N8 -T -RTS -c conf/10000-cluster.yaml --apiPort 8000 --disablePersistence' C-m
+tmux send-keys './bin/kadenaserver +RTS -N4 -T -RTS -c conf/10000-cluster.yaml --disablePersistence --apiPort 8000 2>&1 | tee -a log/10000.log ' C-m
 sleep 1
 tmux split-window -v -p 75
-tmux send-keys 'stack exec -- kadenaserver +RTS -N8 -T -RTS -c conf/10001-cluster.yaml --apiPort 8001 --disablePersistence' C-m
+tmux send-keys './bin/kadenaserver +RTS -N4 -T -RTS -c conf/10001-cluster.yaml --disablePersistence --apiPort 8001 2>&1 | tee -a log/10001.log ' C-m
 sleep 1
 tmux split-window -v -p 66
-tmux send-keys 'stack exec -- kadenaserver +RTS -N8 -T -RTS -c conf/10002-cluster.yaml --apiPort 8002 --disablePersistence' C-m
+tmux send-keys './bin/kadenaserver +RTS -N4 -T -RTS -c conf/10002-cluster.yaml --disablePersistence --apiPort 8002 2>&1 | tee -a log/10002.log ' C-m
 sleep 1
 tmux split-window -v -p 50
-tmux send-keys 'stack exec -- kadenaserver +RTS -N8 -T -RTS -c conf/10003-cluster.yaml --apiPort 8003 --disablePersistence' C-m
+tmux send-keys './bin/kadenaserver +RTS -N4 -T -RTS -c conf/10003-cluster.yaml --disablePersistence --apiPort 8003 2>&1 | tee -a log/10003.log ' C-m
 sleep 1
 tmux select-pane -L
 tmux send-keys './kadenaclient.sh'
--- kadenaclient.sh
+++ kadenaclient.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-stack exec -- rlwrap -A kadenaclient -c "conf/$(ls conf | grep -m 1 client)" +RTS -N2
+rlwrap -A bin/kadenaclient -c "conf/$(ls conf | grep -m 1 client)" +RTS -N2

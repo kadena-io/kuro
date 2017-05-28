78d422546f825cbe3b1f30662592c8f609c17c36
Stuart Popejoy <spopejoy@panix.com>
2017-05-28 14:20:53 -0400

    restored WireFormatTest, does not pass in stack

--- tests/WireFormatSpec.hs
+++ tests/WireFormatSpec.hs
@@ -1,15 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-module WireFormatSpec where
+module WireFormatSpec (spec) where
 
 import Test.Hspec
 
 import qualified Data.Map.Strict as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
+import Data.Aeson
+import Data.Text (Text)
+import Data.Maybe
 
 
 import Kadena.Types
+import qualified Pact.Types.Command as Pact
+import Pact.Types.Crypto
+import Pact.Types.RPC
 
 spec :: Spec
 spec = describe "WireFormat RoundTrips" testWireRoundtrip
@@ -20,26 +27,9 @@ testWireRoundtrip = do
     fromWire Nothing keySet cmdSignedRPC1
       `shouldBe`
         (Right $ cmdRPC1 {
-            _cmdProvenance = ReceivedMsg
+            _newProvenance = ReceivedMsg
               { _pDig = _sigDigest cmdSignedRPC1
               , _pOrig = _sigBody cmdSignedRPC1
-              , _pTimeStamp = Nothing}})
-  it "CommandBatch" $
-    fromWire Nothing keySet cmdbSignedRPC
-      `shouldBe`
-        (Right $ cmdbRPC {
-              _cmdbBatch = Commands $ [cmdRPC1', cmdRPC2']
-            , _cmdbProvenance = ReceivedMsg
-              { _pDig = _sigDigest cmdbSignedRPC
-              , _pOrig = _sigBody cmdbSignedRPC
-              , _pTimeStamp = Nothing}})
-  it "CommandResponse" $
-    fromWire Nothing keySet cmdrSignedRPC
-      `shouldBe`
-        (Right $ cmdrRPC {
-            _cmdrProvenance = ReceivedMsg
-              { _pDig = _sigDigest cmdrSignedRPC
-              , _pOrig = _sigBody cmdrSignedRPC
               , _pTimeStamp = Nothing}})
   it "Seq LogEntry" $
     leSeqDecoded `shouldBe` leSeq
@@ -61,6 +51,7 @@ testWireRoundtrip = do
               { _pDig = _sigDigest aeSignedRPC
               , _pOrig = _sigBody aeSignedRPC
               , _pTimeStamp = Nothing}})
+
   it "AppendEntriesResponse" $
     fromWire Nothing keySet aerSignedRPC
       `shouldBe`
@@ -69,6 +60,7 @@ testWireRoundtrip = do
               { _pDig = _sigDigest aerSignedRPC
               , _pOrig = _sigBody aerSignedRPC
               , _pTimeStamp = Nothing}})
+
   it "RequestVote" $
     fromWire Nothing keySet rvSignedRPC
       `shouldBe`
@@ -83,85 +75,64 @@ testWireRoundtrip = do
 -- ##########################################################
 
 -- #######################################################################
--- NodeId's + Keys for Client (10002), Leader (10000) and Follower (10001)
+-- NodeId's + Keys for Leader (10000) and Follower (10001)
 -- #######################################################################
-nodeIdLeader, nodeIdFollower, nodeIdClient :: NodeId
+nodeIdLeader, nodeIdFollower :: NodeId
 nodeIdLeader = NodeId "localhost" 10000 "tcp://127.0.0.1:10000" $ Alias "leader"
 nodeIdFollower = NodeId "localhost" 10001 "tcp://127.0.0.1:10001" $ Alias "follower"
-nodeIdClient = NodeId "localhost" 10002 "tcp://127.0.0.1:10002" $ Alias "client"
 
 privKeyLeader, privKeyFollower, privKeyClient :: PrivateKey
-privKeyLeader = maybe (error "bad leader key") id $ importPrivate "\204m\223Uo|\211.\144\131\&5Xmlyd$\165T\148\&11P\142m\249\253$\216\232\220c"
-privKeyFollower = maybe (error "bad leader key") id $ importPrivate "$%\181\214\b\138\246(5\181%\199\186\185\t!\NUL\253'\t\ENQ\212^\236O\SOP\217\ACK\EOT\170<"
-privKeyClient = maybe (error "bad leader key") id $ importPrivate "8H\r\198a;\US\249\233b\DLE\211nWy\176\193\STX\236\SUB\151\206\152\tm\205\205\234(\CAN\254\181"
+privKeyLeader = fromMaybe (error "bad key") $
+  importPrivate "\204m\223Uo|\211.\144\131\&5Xmlyd$\165T\148\&11P\142m\249\253$\216\232\220c"
+privKeyFollower = fromMaybe (error "bad key") $
+  importPrivate "$%\181\214\b\138\246(5\181%\199\186\185\t!\NUL\253'\t\ENQ\212^\236O\SOP\217\ACK\EOT\170<"
+privKeyClient = fromMaybe (error "bad key") $
+  importPrivate "8H\r\198a;\US\249\233b\DLE\211nWy\176\193\STX\236\SUB\151\206\152\tm\205\205\234(\CAN\254\181"
 
 pubKeyLeader, pubKeyFollower, pubKeyClient :: PublicKey
-pubKeyLeader = maybe (error "bad leader key") id $ importPublic "f\t\167y\197\140\&2c.L\209;E\181\146\157\226\137\155$\GS(\189\215\SUB\199\r\158\224\FS\190|"
-pubKeyFollower = maybe (error "bad leader key") id $ importPublic "\187\182\129\&4\139\197s\175Sc!\237\&8L \164J7u\184;\CANiC\DLE\243\ESC\206\249\SYN\189\ACK"
-pubKeyClient = maybe (error "bad leader key") id $ importPublic "@*\228W(^\231\193\134\239\254s\ETBN\208\RS\137\201\208,bEk\213\221\185#\152\&7\237\234\DC1"
+pubKeyLeader = fromMaybe (error "bad key") $
+  importPublic "f\t\167y\197\140\&2c.L\209;E\181\146\157\226\137\155$\GS(\189\215\SUB\199\r\158\224\FS\190|"
+pubKeyFollower = fromMaybe (error "bad key") $
+  importPublic "\187\182\129\&4\139\197s\175Sc!\237\&8L \164J7u\184;\CANiC\DLE\243\ESC\206\249\SYN\189\ACK"
+pubKeyClient = fromMaybe (error "bad key") $
+  importPublic "@*\228W(^\231\193\134\239\254s\ETBN\208\RS\137\201\208,bEk\213\221\185#\152\&7\237\234\DC1"
 
 keySet :: KeySet
 keySet = KeySet
   { _ksCluster = Map.fromList [(_alias nodeIdLeader, pubKeyLeader),
-                               (_alias nodeIdFollower, pubKeyFollower)]
-  , _ksClient = Map.fromList [(_alias nodeIdClient, pubKeyClient)] }
+                               (_alias nodeIdFollower, pubKeyFollower)] }
 
 -- #####################################
 -- Commands, with and without provenance
 -- #####################################
-cmdbRPC :: CommandBatch
-cmdbRPC = CommandBatch
-  { _cmdbBatch = Commands $ [cmdRPC1, cmdRPC2]
-  , _cmdbProvenance = NewMsg }
-
-cmdbSignedRPC :: SignedRPC
-cmdbSignedRPC = toWire nodeIdClient pubKeyClient privKeyClient cmdbRPC
-
--- these are signed (received) provenance versions
-cmdbRPC' :: CommandBatch
-cmdbRPC' = (\(Right v) -> v) $ fromWire Nothing keySet cmdbSignedRPC
-
-
--- #####################################
--- Commands, with and without provenance
--- #####################################
-cmdRPC1, cmdRPC2 :: Command
-cmdRPC1 = Command
-  { _cmdEntry = CommandEntry "CreateAccount foo"
-  , _cmdClientId = _alias nodeIdClient
-  , _cmdRequestId = RequestId "0"
-  , _cmdCryptoVerified = UnVerified
-  , _cmdProvenance = NewMsg }
-cmdRPC2 = Command
-  { _cmdEntry = CommandEntry "CreateAccount foo"
-  , _cmdClientId = _alias nodeIdClient
-  , _cmdRequestId = RequestId "1"
-  , _cmdCryptoVerified = UnVerified
-  , _cmdProvenance = NewMsg }
+cmdRPC1, cmdRPC2 :: NewCmdRPC
+cmdRPC1 = NewCmdRPC
+          { _newCmd =
+            [encodeCommand $
+              SmartContractCommand
+              { _sccCmd = Pact.mkCommand [(ED25519,privKeyClient,pubKeyClient)] Nothing "nonce1"
+                          (Exec (ExecMsg ("(+ 1 2)" :: Text) Null))
+              , _sccPreProc = Unprocessed
+              }]
+          , _newProvenance = NewMsg }
+cmdRPC2 = NewCmdRPC
+          { _newCmd =
+            [encodeCommand $
+              SmartContractCommand
+              { _sccCmd = Pact.mkCommand [(ED25519,privKeyClient,pubKeyClient)] Nothing "nonce2"
+                          (Exec (ExecMsg ("(+ 3 4)" :: Text) Null))
+              , _sccPreProc = Unprocessed
+              }]
+          , _newProvenance = NewMsg }
 
 cmdSignedRPC1, cmdSignedRPC2 :: SignedRPC
-cmdSignedRPC1 = toWire nodeIdClient pubKeyClient privKeyClient cmdRPC1
-cmdSignedRPC2 = toWire nodeIdClient pubKeyClient privKeyClient cmdRPC2
+cmdSignedRPC1 = toWire nodeIdLeader pubKeyLeader privKeyLeader cmdRPC1
+cmdSignedRPC2 = toWire nodeIdFollower pubKeyFollower privKeyFollower cmdRPC2
 
 -- these are signed (received) provenance versions
 cmdRPC1', cmdRPC2' :: Command
-cmdRPC1' = (\(Right v) -> v) $ fromWire Nothing keySet cmdSignedRPC1
-cmdRPC2' = (\(Right v) -> v) $ fromWire Nothing keySet cmdSignedRPC2
-
--- #############################################
--- CommandResponse, with and without provenance
--- #############################################
-cmdrRPC :: CommandResponse
-cmdrRPC = CommandResponse
-  { _cmdrResult     = CommandResult "account created: foo"
-  , _cmdrNodeId     = nodeIdLeader
-  , _cmdrRequestId  = RequestId "1"
-  , _cmdrLatency    = 1
-  , _cmdrProvenance = NewMsg
-  }
-
-cmdrSignedRPC :: SignedRPC
-cmdrSignedRPC = toWire nodeIdLeader pubKeyLeader privKeyLeader cmdrRPC
+cmdRPC1' = either error (decodeCommand . head . _newCmd) $ fromWire Nothing keySet cmdSignedRPC1
+cmdRPC2' = either error (decodeCommand . head . _newCmd) $ fromWire Nothing keySet cmdSignedRPC2
 
 -- ########################################################
 -- LogEntry(s) and Seq LogEntry with correct hashes.
@@ -173,21 +144,23 @@ logEntry1 = hashLogEntry Nothing $ LogEntry
   { _leTerm    = Term 0
   , _leLogIndex = 0
   , _leCommand = cmdRPC1'
-  , _leHash    = ""
+  , _leHash    = hash "logEntry1"
+  , _leCmdLatMetrics = Nothing
   }
-logEntry2 = hashLogEntry (Just logEntry1) $ LogEntry
+logEntry2 = hashLogEntry (Just $ _leHash logEntry1) $ LogEntry
   { _leTerm    = Term 0
   , _leLogIndex = 1
   , _leCommand = cmdRPC2'
-  , _leHash    = ""
+  , _leHash    = hash "logEntry2"
+  , _leCmdLatMetrics = Nothing
   }
 
 leSeq, leSeqDecoded :: LogEntries
 leSeq = (\(Right v) -> v) $ checkLogEntries $ Map.fromList [(0,logEntry1),(1,logEntry2)]
-leSeqDecoded = (\(Right v) -> v) $ decodeLEWire Nothing keySet leWire
+leSeqDecoded = (\(Right v) -> v) $ decodeLEWire Nothing leWire
 
 leWire :: [LEWire]
-leWire = encodeLEWire nodeIdLeader pubKeyLeader privKeyLeader leSeq
+leWire = encodeLEWire leSeq
 
 -- ################################################
 -- RequestVoteResponse, with and without provenance
@@ -239,11 +212,10 @@ aeRPC = AppendEntries
   , _aeProvenance  = NewMsg
   }
 
+
 aeSignedRPC :: SignedRPC
 aeSignedRPC = toWire nodeIdLeader pubKeyLeader privKeyLeader aeRPC
 
-aeRPC' :: RequestVoteResponse
-aeRPC' = (\(Right v) -> v) $ fromWire Nothing keySet aeSignedRPC
 
 -- #####################
 -- AppendEntriesResponse
@@ -255,12 +227,13 @@ aerRPC = AppendEntriesResponse
   , _aerSuccess    = True
   , _aerConvinced  = True
   , _aerIndex      = LogIndex 1
-  , _aerHash       = "\244\136\187c\222\164\131\178;D)M\DEL\142|\251Kv\213\186\247q;3`\194\227O\US\223Q\157"
+  , _aerHash       = hash "hello"
   , _aerProvenance = NewMsg
   }
 
 aerSignedRPC :: SignedRPC
 aerSignedRPC = toWire nodeIdFollower pubKeyFollower privKeyFollower aerRPC
+
 
 -- ###########
 -- RequestVote

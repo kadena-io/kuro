8e56724950d23fa0bb67cea1172bc0b03f35c682
Will Martino <wjmartino@gmail.com>
2017-06-16 23:40:43 -0400

    develop: got bound-2 to build Signed-off-by: Will Martino <wjmartino@gmail.com>

--- scripts/build-beta-distro.ubuntu-16.04.sh
+++ scripts/build-beta-distro.ubuntu-16.04.sh
@@ -22,13 +22,13 @@ safe cp ./bin/genconfs ./kadena-beta/bin/osx/;
 safe cp ./bin/kadenaserver ./kadena-beta/bin/osx/;
 safe cp ./bin/kadenaclient ./kadena-beta/bin/osx/;
 
-#chirp "Builing and Copying: Ubuntu"
-#rm -rf ./kadena-beta/bin/ubuntu-16.04/{genconfs,kadenaserver,kadenaclient}
-#safe docker build --cpuset-cpus="0-3" --cpu-shares=1024 --memory=8g -t kadena-base:ubuntu-16.04 -f docker/ubuntu-base.Dockerfile .
-#safe docker build --cpuset-cpus="0-3" --cpu-shares=1024 --memory=8g -t kadena:ubuntu-16.04 -f docker/ubuntu-build.Dockerfile .
-#safe docker run -i -v ${PWD}:/work_dir kadena:ubuntu-16.04 << COMMANDS
-#cp -R /ubuntu-16.04 /work_dir/kadena-beta/bin
-#COMMANDS
+chirp "Builing and Copying: Ubuntu"
+rm -rf ./kadena-beta/bin/ubuntu-16.04/{genconfs,kadenaserver,kadenaclient}
+safe docker build --cpuset-cpus="0-3" --cpu-shares=1024 --memory=8g -t kadena-base:ubuntu-16.04 -f docker/ubuntu-base.Dockerfile .
+safe docker build --cpuset-cpus="0-3" --cpu-shares=1024 --memory=8g -t kadena:ubuntu-16.04 -f docker/ubuntu-build.Dockerfile .
+safe docker run -i -v ${PWD}:/work_dir kadena:ubuntu-16.04 << COMMANDS
+cp -R /ubuntu-16.04 /work_dir/kadena-beta/bin
+COMMANDS
 safe cp docker/ubuntu-base.Dockerfile kadena-beta/docker/
 
 chirp "Builing and Copying: Performance Monitor"

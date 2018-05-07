69be6aa68fb922cf1bf1464bf03c1eae7aff55bd
Doug Beardsley <mightybyte@gmail.com>
2018-05-07 17:31:12 -0400

    Clean up and point to latest working pact

--- default.nix
+++ default.nix
@@ -23,6 +23,13 @@ in
             katip = pkgs.haskell.lib.doJailbreak (self.callHackage "katip" "0.3.1.4" {});
             ridley = pkgs.haskell.lib.dontCheck (self.callHackage "ridley" "0.3.1.2" {});
 
+            criterion = pkgs.haskell.lib.dontCheck (self.callCabal2nix "criterion" (pkgs.fetchFromGitHub {
+              owner = "bos";
+              repo = "criterion";
+              rev = "5a704392b670c189475649c32d05eeca9370d340";
+              sha256 = "1kp0l78l14w0mmva1gs9g30zdfjx4jkl5avl6a3vbww3q50if8pv";
+            }) {});
+
             # Version 1.6.4, needed by weeder, not in callHackage yet
             extra = pkgs.haskell.lib.dontCheck (self.callCabal2nix "extra" (pkgs.fetchFromGitHub {
               owner = "ndmitchell";
@@ -31,26 +38,35 @@ in
               sha256 = "1p7rc5m70rkm1ma8gnihfwyxysr0n3wxk8ijhp6qjnqp5zwifhhn";
             }) {});
 
+            # Old version because the kadena stack.yaml hasn't been updated in awhile
+            # TODO Get rid of doJailbreak and dontCheck
+            pact = self.callCabal2nix "pact" (pkgs.fetchFromGitHub {
+              owner = "kadena-io";
+              repo = "pact";
+              rev = "b5e5719d8baef1100d56c102dd4e676ea72c8d90";
+              sha256 = "1gw2c6myqa6wga2glkyb47kkmiq5165i4m627b8rnmyjxd39y10h";
+            }) {};
+
+            pact-persist = pkgs.haskell.lib.doJailbreak (self.callCabal2nix "pact-persist" (builtins.fetchGit {
+              name = "pact-persist";
+              url = ssh://git@github.com/kadena-io/pact-persist.git;
+              rev = "2a4b1d333dea669038f10f30ab9b64aab2afd6b0";
+            }) {});
+
+            # dontCheck is here because a couple tests were failing
+            # TODO open issues for these test failures
+            statistics = pkgs.haskell.lib.dontCheck (self.callCabal2nix "statistics" (pkgs.fetchFromGitHub {
+              owner = "bos";
+              repo = "statistics";
+              rev = "1ed1f2844c5a2209f5ea72e60df7d14d3bb7ac1a";
+              sha256 = "1jjmdhfn198pfl3k5c4826xddskqkfsxyw6l5nmwrc8ibhhnxl7p";
+            }) {});
+
             thyme = pkgs.haskell.lib.dontCheck (self.callCabal2nix "thyme" (pkgs.fetchFromGitHub {
               owner = "kadena-io";
               repo = "thyme";
               rev = "6ee9fcb026ebdb49b810802a981d166680d867c9";
               sha256 = "09fcf896bs6i71qhj5w6qbwllkv3gywnn5wfsdrcm0w1y6h8i88f";
-            }) {});
-
-            # Old version because the kadena stack.yaml hasn't been updated in awhile
-            # TODO Get rid of doJailbreak and dontCheck
-            pact = pkgs.haskell.lib.doJailbreak (pkgs.haskell.lib.dontCheck (self.callCabal2nix "pact" (pkgs.fetchFromGitHub {
-              owner = "kadena-io";
-              repo = "pact";
-              rev = "d1f2892bc739c1f8fb26b59b4e7528a592d5d12d";
-              sha256 = "1j8k7vh2iz0r44a5r1h94p6ajg96hi1sn8wsclvv5mszwfcqcmh5";
-            }) {}));
-            # pact = pkgs.haskell.lib.dontCheck (self.callCabal2nix "pact" ../pact {});
-            pact-persist = pkgs.haskell.lib.doJailbreak (self.callCabal2nix "pact-persist" (builtins.fetchGit {
-              name = "pact-persist";
-              url = ssh://git@github.com/kadena-io/pact-persist.git;
-              rev = "2a4b1d333dea669038f10f30ab9b64aab2afd6b0";
             }) {});
 
             # weeder = self.callHackage "weeder" "1.0.5" {};
@@ -62,20 +78,11 @@ in
             }) {};
           };
     packages = {
-      # pact = ../pact;
-      # pact = pkgs.fetchFromGitHub {
-      #   owner = "kadena-io";
-      #   repo = "pact";
-      #   rev = "32a0b461a30ecd08b38a10155908a6969d2f42bc";
-      #   sha256 = "1v2nddx90zxx1akmiy0j3z460qxgm53088pimzspma9hcjny61d9";
-      # };
-
       kadena = ./.;
     };
     
     shells = {
-      ghc = ["pact" "kadena"];
-      # ghcjs = ["pact" "pact-ghcjs"];
+      ghc = ["kadena"];
     };
   
   })

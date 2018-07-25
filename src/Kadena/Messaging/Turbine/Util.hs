a822c45ca060f1cf35d025c116123bc6392a9519
Emily Pillmore <emily@kadena.io>
2018-07-25 15:29:23 -0400

    Turbine.Types -> Types.Turbine

---
+++ src/Kadena/Messaging/Turbine/Util.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Kadena.Messaging.Turbine.Util
+  ( turbineRv
+  , turbineAer
+  , turbineCmd
+  , turbineGeneral
+  , parallelVerify
+  , parSeqToList
+  , processMsg
+  ) where
+
+import Control.Parallel.Strategies
+
+import Data.Sequence (Seq)
+import qualified Data.Sequence as Seq
+
+import Kadena.Message
+import Kadena.Types hiding (debugPrint)
+import Kadena.Types.KeySet
+
+turbineRv, turbineAer, turbineCmd, turbineGeneral :: String
+turbineRv = "[Turbine|Rv]: "
+turbineAer = "[Turbine|Aer]: "
+turbineCmd = "[Turbine|Cmd]: "
+turbineGeneral = "[Turbine|Gen]: "
+
+parallelVerify :: (f -> (ReceivedAt,SignedRPC)) -> KeySet -> Seq f -> [Either String RPC]
+parallelVerify f ks msgs = runEval $ parSeqToList (processMsg f ks) msgs
+
+parSeqToList :: (f -> Either String RPC) -> Seq f -> Eval [Either String RPC]
+parSeqToList f s = do
+  case Seq.viewl s of
+    Seq.EmptyL -> return []
+    x Seq.:< xs -> do
+      res <- rpar (f x)
+      (res:) <$> parSeqToList f xs
+
+processMsg :: (f -> (ReceivedAt,SignedRPC)) -> KeySet -> (f -> Either String RPC)
+processMsg f ks = (\(ts, msg) -> signedRPCtoRPC (Just ts) ks msg) . f

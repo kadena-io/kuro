a0e562bba183c1be798b3d1f9ff8e20734d9f938
Doug Beardsley <mightybyte@gmail.com>
2018-08-06 17:58:13 -0400

    Take unnecessary stuff out of nix hash

--- default.nix
+++ default.nix
@@ -85,7 +85,10 @@ in
             }) {};
           };
     packages = {
-      kadena = ./.;
+      kadena = builtins.filterSource
+        (path: type: !(builtins.elem (baseNameOf path)
+           ["result" "dist" "dist-ghcjs" ".git"]))
+        ./.;
     };
     
     shells = {

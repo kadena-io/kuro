ff2daa8a86917b96e87632b61105d07a7030d9da
Linda Ortega Cordoves <linda.ortega2013@gmail.com>
2018-09-20 16:33:36 +0000

    Server monitoring with aws and tmux

--- scripts/aws/start_aws.sh
+++ scripts/aws/start_aws.sh
@@ -4,4 +4,27 @@ OS='ubuntu-16.04'
 
 sudo chmod +x /home/ubuntu/kadena-beta/bin/$OS/kadenaclient
 ansible-playbook /home/ubuntu/kadena/scripts/aws/run_servers.yml &&
-rlwrap -A /home/ubuntu/kadena-beta/bin/$OS/kadenaclient -c "/home/ubuntu/conf/$(ls conf | grep -m 1 client)" +RTS -N2
+STR_SERVERS=`cat /home/ubuntu/ipAddr.yml`
+SERVERS=( $STR_SERVERS )
+if [ ${#SERVERS[*]} -ne 4 ]
+then
+  echo "Received ${#SERVERS[@]} kadenaservers but expected 4"
+  exit 1
+fi
+
+tmux new-window
+
+tmux split-window -h
+tmux send-keys "ssh -t -A ubuntu@${SERVERS[0]} tail -f log/node.log" C-m
+
+tmux split-window -v -p 75
+tmux send-keys "ssh -t -A ubuntu@${SERVERS[1]} tail -f log/node.log" C-m
+
+tmux split-window -v -p 66
+tmux send-keys "ssh -t -A ubuntu@${SERVERS[2]} tail -f log/node.log" C-m
+
+tmux split-window -v -p 50
+tmux send-keys "ssh -t -A ubuntu@${SERVERS[3]} tail -f log/node.log" C-m
+
+tmux select-pane -L
+tmux send-keys "rlwrap -A /home/ubuntu/kadena-beta/bin/$OS/kadenaclient -c /home/ubuntu/conf/client.yaml +RTS -N2"

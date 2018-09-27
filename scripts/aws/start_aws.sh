4077322546dfdc6f62516e5662d806b47620ee56
Linda Ortega Cordoves <linda.ortega2013@gmail.com>
2018-09-27 22:20:48 +0000

    Edited playbooks to work across multiple distributions

--- scripts/aws/start_aws.sh
+++ scripts/aws/start_aws.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
 OS='ubuntu-16.04'
+EC2_USER='ubuntu'
 
-sudo chmod +x /home/ubuntu/kadena-beta/bin/$OS/kadenaclient
-ansible-playbook /home/ubuntu/kadena/scripts/aws/run_servers.yml &&
-STR_SERVERS=`cat /home/ubuntu/ipAddr.yml`
+sudo chmod +x bin/$OS/kadenaclient
+ansible-playbook aws/run_servers.yml &&
+STR_SERVERS=`cat aws/ipAddr.yml`
 SERVERS=( $STR_SERVERS )
 if [ ${#SERVERS[*]} -ne 4 ]
 then
-  echo "Received ${#SERVERS[@]} kadenaservers but expected 4"
+  echo "Received ${#SERVERS[@]} kadenaservers but the demo requires 4."
   exit 1
 fi
 
 tmux new-window
 
 tmux split-window -h
-tmux send-keys "ssh -t -A ubuntu@${SERVERS[0]} tail -f log/node.log" C-m
+tmux send-keys "ssh -t -A $EC2_USER@${SERVERS[0]} tail -f log/node.log" C-m
 
 tmux split-window -v -p 75
-tmux send-keys "ssh -t -A ubuntu@${SERVERS[1]} tail -f log/node.log" C-m
+tmux send-keys "ssh -t -A $EC2_USER@${SERVERS[1]} tail -f log/node.log" C-m
 
 tmux split-window -v -p 66
-tmux send-keys "ssh -t -A ubuntu@${SERVERS[2]} tail -f log/node.log" C-m
+tmux send-keys "ssh -t -A $EC2_USER@${SERVERS[2]} tail -f log/node.log" C-m
 
 tmux split-window -v -p 50
-tmux send-keys "ssh -t -A ubuntu@${SERVERS[3]} tail -f log/node.log" C-m
+tmux send-keys "ssh -t -A $EC2_USER@${SERVERS[3]} tail -f log/node.log" C-m
 
 tmux select-pane -L
-tmux send-keys "rlwrap -A /home/ubuntu/kadena-beta/bin/$OS/kadenaclient -c /home/ubuntu/conf/client.yaml +RTS -N2"
+tmux send-keys "rlwrap -A bin/$OS/kadenaclient -c conf/client.yaml +RTS -N2"

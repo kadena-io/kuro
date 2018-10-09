6514375824c58abab3bf6d4f3670fc3b4ef6ed67
Linda Ortega Cordoves <linda.ortega2013@gmail.com>
2018-10-09 13:46:01 -0400

    Fixes file not found error with demo

--- demo/start.sh
+++ demo/start.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 rm ./log/*.sqlite* ./log/10000.log ./log/10001.log ./log/10002.log ./log/10003.log ./log/access.log ./log/error.log ./log/client.log
-touch ./log/access.log ./log/error.log
+touch ./log/access.log ./log/error.log ./log/10000.log ./log/10001.log ./log/10002.log ./log/10003.log
 tmux new-window
 tmux split-window -h
 tmux send-keys './bin/kadenaserver +RTS -N4 -RTS -c conf/10000-cluster.yaml &' C-m
-tmux send-keys 'tail -f log/10000.log' C-m
+tmux send-keys 'tail -f ./log/10000.log' C-m
 sleep 1
 tmux split-window -v -p 75
 tmux send-keys './bin/kadenaserver +RTS -N4 -RTS -c conf/10001-cluster.yaml &' C-m
-tmux send-keys 'tail -f log/10001.log' C-m
+tmux send-keys 'tail -f ./log/10001.log' C-m
 sleep 1
 tmux split-window -v -p 66
 tmux send-keys './bin/kadenaserver +RTS -N4 -RTS -c conf/10002-cluster.yaml &' C-m
-tmux send-keys 'tail -f log/10002.log' C-m
+tmux send-keys 'tail -f ./log/10002.log' C-m
 sleep 1
 tmux split-window -v -p 50
 tmux send-keys './bin/kadenaserver +RTS -N4 -RTS -c conf/10003-cluster.yaml &' C-m
-tmux send-keys 'tail -f log/10003.log' C-m
+tmux send-keys 'tail -f ./log/10003.log' C-m
 sleep 1
 tmux select-pane -L
 tmux send-keys './kadenaclient.sh'

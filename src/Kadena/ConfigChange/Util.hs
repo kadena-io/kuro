da32361ac986031d870aa99e45f3168ffe3e3e83
Mark Nichols <marklnichols@gmail.com>
2019-06-19 12:13:34 -0400

    Possible merge checkpoint

--- src/Kadena/ConfigChange/Util.hs
+++ src/Kadena/ConfigChange/Util.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+
 module Kadena.ConfigChange.Util
   ( getMissingKeys
   ) where
@@ -8,13 +10,17 @@ import Kadena.Config.TMVar
 import Kadena.Types.Base
 
 import Pact.Bench (eitherDie)
+import qualified Pact.Types.Command as P (UserSig)
 import Pact.Types.Command
+import qualified Pact.Types.Crypto as P (Scheme, PublicKey, PrivateKey, Signature)
+import qualified Pact.Types.Scheme as P (PPKScheme(..), SPPKScheme)
 import Pact.Types.Util (fromText')
 
-getMissingKeys :: Config -> [UserSig]-> IO [Alias]
-getMissingKeys cfg sigs = do
+-- TODO: [UserSig] needs to become Payload...
+getMissingKeys :: P.Scheme s => s -> Config -> [UserSig] -> IO [Alias]
+getMissingKeys scheme cfg sigs = do
   let textKeys = fmap _usPubKey sigs
-  pubKeys <- traverse (eitherDie . fromText') textKeys :: IO [PublicKey]
+  pubKeys <- traverse (eitherDie . fromText') textKeys :: IO [P.PublicKey s]
   let filtered = filter f (Map.toList (_adminKeys cfg)) where
         f (_, k) = notElem k pubKeys
   return $ fmap fst filtered
